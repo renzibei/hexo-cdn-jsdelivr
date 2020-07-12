461a40a108910a8f3873116b2eccb17989ed8a25
Fan Qu <fan_qu@icloud.com>
2020-07-12 23:37:19 +0800

    complete basic functions

--- index.js
+++ index.js
@@ -2,13 +2,16 @@
 'use strict';
 
 
-
+hexo.extend.helper.register('cdn_css', require('./lib/helper').cdn_css);
+hexo.extend.helper.register('cdn_js', require('./lib/helper').cdn_js);
+hexo.extend.helper.register('cdn_asset', require('./lib/helper').cdn_asset);
 var cdnConfig = require('./lib/process').getCdnConfig(hexo);
 
 if (cdnConfig && 'use_cdn' in cdnConfig && cdnConfig.use_cdn) {
   hexo.extend.filter.register('before_post_render', require('./lib/process').processPost);
-  hexo.extend.generator.register('jsdelivr_cdn_generator',  require('./lib/generator'));
-  console.log("use cdn");
+  hexo.extend.generator.register('jsdelivr_cdn_generator',  require('./lib/generator').assetsGenerator);
+  hexo.extend.filter.register('before_exit', require('./lib/generator').assetsDeployer);
+  hexo.log.info("use cdn");
 }
 else {
   console.log("not use cdn");
